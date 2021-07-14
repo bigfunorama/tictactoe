2090690503bb5cf13c7b8c6aed776945fa2dfec1
bigfunorama <jay@lagrangesystems.com>
2021-07-14 17:03:06 -0600

    added to the plotter to make lots of comparisons between columns in session data.

--- cmd/plotting/server.go
+++ cmd/plotting/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"encoding/csv"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -42,8 +43,58 @@ func plotHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, out)
 }
 
-func readCSV(fname string, xlabel, ylabel, zlabel) []byte {
-	
+func checkLabels(xlabel, ylabel, zlabel string) error {
+	out := "missing labels: "
+	if xlabel == "" {
+		out += "xlabel "
+	}
+	if ylabel == "" {
+		out += "ylabel "
+	}
+	if zlabel == "" {
+		out += "zlabel "
+	}
+
+	if out != "missing labels: " {
+		return fmt.Errorf(out)
+	}
+	return nil
+}
+
+func readCSV(fname, xlabel, ylabel, zlabel string) (string, error) {
+	log.Println("loading input, ", fname)
+	f, err := os.Open(fname)
+	if err != nil {
+		return "", err
+	}
+	in := bufio.NewReader(f)
+	cc := csv.NewReader(in)
+	keys, err := cc.Read()
+	err = checkLabels(xlabel, ylabel, zlabel)
+	if err != nil {
+		return "", err
+	}
+	cols := []int{-1, -1, -1}
+	for idx := 0; idx < len(keys); idx++ {
+		if keys[idx] == xlabel {
+			cols[0] = idx
+		}
+		if keys[idx] == ylabel {
+			cols[1] = idx
+		}
+		if keys[idx] == zlabel {
+			cols[2] = idx
+		}
+	}
+
+	out := xlabel + "," + ylabel + "," + zlabel + "\n"
+	row, err := cc.Read()
+	for err == nil {
+		rr := row[cols[0]] + "," + row[cols[1]] + "," + row[cols[2]] + "\n"
+		out += rr
+		row, err = cc.Read()
+	}
+	return out, nil
 }
 
 func dataHandler(w http.ResponseWriter, r *http.Request) {
@@ -61,7 +112,15 @@ func dataHandler(w http.ResponseWriter, r *http.Request) {
 	xlabel := vals.Get("x")
 	ylabel := vals.Get("y")
 	zlabel := vals.Get("z")
-	out, err := readCSV("./test.csv", xlabel, ylabel, zlabel)
+
+	var out string
+	var err error
+	e := checkLabels(xlabel, ylabel, zlabel)
+	if e != nil {
+		out, err = readFile("./test.csv")
+	} else {
+		out, err = readCSV("./test.csv", xlabel, ylabel, zlabel)
+	}
 	if err != nil {
 		http.Error(w, "internal error, "+err.Error(), http.StatusInternalServerError)
 	}
